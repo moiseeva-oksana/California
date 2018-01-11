9f9e1ce0c28660686193f11ef414ca70cf0b4dcc
Oksana Moiseeva <moiseeva.ox@gmail.com>
2018-01-11 19:03:02 +0300

    Initial commit

---
+++ src/main/java/com/example/ec/CaliforniaApplication.java
@@ -0,0 +1,12 @@
+package com.example.ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaliforniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaliforniaApplication.class, args);
+	}
+}
