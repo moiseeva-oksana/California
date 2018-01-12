4fb120a332840b219317dbdf307c4ab8f686b35b
Oksana Moiseeva <moiseeva.ox@gmail.com>
2018-01-12 22:14:11 +0300

    TourRating controller

---
+++ src/main/java/com/example/ec/repo/TourRatingRepository.java
@@ -0,0 +1,14 @@
+package com.example.ec.repo;
+
+import com.example.ec.domain.TourRating;
+import com.example.ec.domain.TourRatingPk;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+@RepositoryRestResource(exported = false)
+public interface TourRatingRepository extends CrudRepository<TourRating, TourRatingPk> {
+    List<TourRating> findByPkTourId(Integer tourId);
+
+    TourRating findByPkTourIdAndPkCustomerId(Integer tourId, Integer customerId);
+}
